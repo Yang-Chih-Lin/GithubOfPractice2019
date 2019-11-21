91fe5546a8acc69076e76f34843b236c5a8afe77
Yang-Chih-Lin <yangchih1105@gmail.com>
2019-11-21 18:03:53 +0800

    js 3.Operators 課程練習

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-        //this is my first javascript
+        /*//this is my first javascript
         console.log('hello :))  ------- :))');
         let name = 'YangChih';
         console.log(name);
@@ -14,9 +14,39 @@
         }
 
         greet('John');
-
         function square(number){
                 return number * number;
-        }
+        }*/
 
-        console.log(square(13));
+        // console.log(square(13));
+
+        /*console.log('1' === 1);//嚴格的等於Type與Value都要相等 -> false
+        console.log('1' == 1);//嚴格的等於Type與Value都要相等 -> true
+
+        let points = 110;
+        let type = points > 100 ? 'gold>100' : 'silver<100';
+        console.log(type);
+        
+        // 0,undefined,null -> false
+        */
+
+        /*var X = 10;
+        console.log(X);
+        console.log(++X);
+        console.log(X++);
+        console.log(X++);
+        console.log(X++);*/
+
+
+        /*11- Quiz
+        1. b +-得負
+        2. a 負或正 -> 正
+        3. c X大於10才顯示1
+        4. 5*9=45
+        5. a 之Type不相同
+        6. b x=true
+        7. 比大小的結果為布林值
+        8. 5+3=8
+        9. y = ++x; 時 =11 
+        10. 1 !==2
+        */
