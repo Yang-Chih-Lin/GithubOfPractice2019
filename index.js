1f10523f53f39899f1b72d500fd7c37580207c12
Yang-Chih-Lin <yangchih1105@gmail.com>
2019-11-21 17:27:29 +0800

    js 2.Basics 課程練習

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
         //this is my first javascript
         console.log('hello :))  ------- :))');
+        let name = 'YangChih';
+        console.log(name);
+        //names should be meaningful
+        let firstName = "Yang Chih";
+        let lastName = "Lin";
+
+        const interestRate = 0.3;//constant:1 不會改變的值
+        //interestRate = 1;//會出現error
+
+        function greet(name0){
+                console.log('Helli World ' + name0);
+        }
+
+        greet('John');
+
+        function square(number){
+                return number * number;
+        }
+
+        console.log(square(13));
